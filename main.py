b47002ffd433eb34da8dea3d749bde40249bbfd3
Abdulmunim Abdulsalam <abdulsalamabdulmunim@gmail.com>
2025-07-31 06:58:42 +0100

    Added tasks code

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
@@ -6,3 +8,47 @@ app = FastAPI()
 
 def hello():
     return {"message": "Hello FastAPI!"}
+
+# Indicates how the task should look like
+class Task(BaseModel):
+    id: int
+    title: str
+    completed: bool = False
+
+# List to store tasks
+tasks: List[Task] = []
+
+# Get all tasks
+@app.get("/tasks", response_model=List[Task])
+def get_tasks():
+    return tasks
+
+# Create a new task
+@app.post("/tasks", response_model=List[Task])
+def create_task(task: Task):
+    # check if task with the same id already exists
+    for current_task in tasks:
+        if current_task.id == task.id:
+            raise HTTPException(status_code=400, detail="Task with this ID already exists")
+    # add the new task to the list
+    tasks.append(task)
+    return tasks
+
+# Update an existing task
+@app.put("/tasks/{task_id}", response_model= List[Task])
+def update_task(task_id: int, updated_task: Task):
+    for index, current_task in enumerate(tasks):
+        if current_task.id == task_id:
+          tasks[index] = update_task
+          return update_task 
+    raise HTTPException(status_code=404, detail="Task not found")
+
+# Delete a task
+@app.delete("/tasks/{task_id}", response_model= List[Task])
+def delete_task(task_id: int):
+    for index, current_task in enumerate(tasks):
+        if current_task.id == task_id:
+            del tasks[index]
+            return tasks
+        raise HTTPException(status_code=404, detail="Task not found")  
+
